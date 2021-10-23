00ee35e380917e4d5be3e755233de5fdf40d8c80
fefe <fedescalada17@hotmail.com>
2021-10-22 23:36:01 -0300

    devuelve algo

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function addDesafio(params = {}){
 function getDesafio(){
 
     
-    return fetch("https://cdn.contentful.com/spaces/96le97k9rsgn/environments/master/content_types?access_token=tHLOhPQdV9e9m5erEByoksWU_yGaj0eYQNOHzBR0vvc&content_type={desafio}")
+    return fetch("https://cdn.contentful.com/spaces/96le97k9rsgn/environments/master/entries?access_token=tHLOhPQdV9e9m5erEByoksWU_yGaj0eYQNOHzBR0vvc&content_type=desafio")
     .then(res =>{
         return res.json()
         
