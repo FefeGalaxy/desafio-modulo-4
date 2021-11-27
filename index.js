8172e363a548c04824cd0bf837122e495da042ad
fefe <fedescalada17@hotmail.com>
2021-11-27 00:02:51 -0300

    no funciona

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
-//function getDesafio(){
+function addTitulo(params = {}){
+  const template = document.querySelector("#section-1__titulo-template");
+  const titulo = document.querySelector(".section-1__titulo");
 
+  template.content.querySelector(".section-1__titulo-1").textContent =
+  params.contentful;
+
+  const clone = document.importNode(template.content, true);
+  titulo.appendChild(clone);
+
+}
+
+function getDesafio(){
     
-    //return fetch("https://cdn.contentful.com/spaces/96le97k9rsgn/environments/master/entries?access_token=tHLOhPQdV9e9m5erEByoksWU_yGaj0eYQNOHzBR0vvc&content_type=desafio")
-    //.then(res =>{
-      //  return res.json()
-        
-        
-    //}).then((json)=>{
-        
-      //  const fieldsCollections = json.items.map((item)=>{
+    return fetch("https://cdn.contentful.com/spaces/96le97k9rsgn/environments/master/entries?access_token=tHLOhPQdV9e9m5erEByoksWU_yGaj0eYQNOHzBR0vvc&content_type=desafio")
+    .then(res =>{
+      return res.json()
+    }).then((data)=>{
+      const fieldsCollections = data.items.map((item)=>{
+        return{
+          title: item.fields.contentful
 
-            
-        //    return{
-          //      title: item.fields.titulo
-            //}
+        }
+      })
+      return fieldsCollections
+    })
+  
 
-        //})
-
-        //return fieldsCollections;
-    //});
+}
 
 
-
-
-//}
+function main(){
+  getDesafio().then(function(desafio){
+    for( const d of desafio){
+      addTitulo(d);
+    }
+  });
+}
+main();
