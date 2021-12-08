579d8b7b6ba666f98b39b48773b22f5c94350715
fefe <fedescalada17@hotmail.com>
2021-12-08 19:20:11 -0300

    email

--- portfolio.js
+++ portfolio.js
@@ -2,10 +2,8 @@ function main () {
     const headerEl = document.querySelector(".header-content");
     headerContent(headerEl);
 
-    const portfolioEL = document.querySelector(".portfolio-content");
-    portfolioContent(portfolioEL);
-
     const footerEl = document.querySelector(".footer-content");
     footerContent(footerEl);
 }
+
 main();
--- servicios.js
+++ servicios.js
@@ -1,13 +1,9 @@
-(function(){
-    const botonAbrir = document.querySelector(".servicios__header-img");
-    const botonCerrar = document.querySelector(".header__ventana-cierra");
-    const ventana = document.querySelector(".header__ventana");
-   
-    botonAbrir.addEventListener("click", ()=>{
-       ventana.style.display = "inherit";
-    });
-    botonCerrar.addEventListener("click", ()=>{
-       ventana.style.display = "";
-    });
-   
-   })();
+function main () {
+   const headerEl = document.querySelector(".header-content");
+   headerContent(headerEl);
+
+   const footerEl = document.querySelector(".footer-content");
+   footerContent(footerEl);
+}
+
+main();
