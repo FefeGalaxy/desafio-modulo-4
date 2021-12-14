63c0f355ecea191acc4e6634393f59a6955ef4a8
fefe <fedescalada17@hotmail.com>
2021-12-14 16:05:34 -0300

    contentful index

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-function getDesafio(){
+//function getDesafio(){
     
-  return fetch("https://cdn.contentful.com/spaces/96le97k9rsgn/environments/master/entries?access_token=tHLOhPQdV9e9m5erEByoksWU_yGaj0eYQNOHzBR0vvc&content_type=desafio")
-  .then(res =>{
-    return res.json()
-  }).then((data)=>{
-    const fieldsCollections = data.items.map((item)=>{
-      return{
-        contentful: item.fields.contentful
+  //return fetch("https://cdn.contentful.com/spaces/96le97k9rsgn/environments/master/entries?access_token=tHLOhPQdV9e9m5erEByoksWU_yGaj0eYQNOHzBR0vvc&content_type=desafio")
+  //.then(res =>{
+    //return res.json()
+  //}).then((data)=>{
+    //const fieldsCollections = data.items.map((item)=>{
+      //return{
+        //contentful: item.fields.contentful
 
-      }
-  })
-    return fieldsCollections
-  })
+      //}
+  //})
+   // return fieldsCollections
+  //})
 
-}
+//}
 
-function addDesafio( params = {}){
-  const template = document.querySelector("#section-1__template");
-  const container = document.querySelector(".section-1__titulo");
+//function addDesafio( params = {}){
+  //const container = document.querySelector(".section-1__titulo");
 
-  template.content.querySelector(".section-1__titulo-1").textContent =
-   params.contentful;
-  template.content.querySelector(".section-1__titulo-2").textContent =
-   params.contentful;
+  //container.content.querySelector(".section-1__titulo-1").textContent =
+  // params.contentful;
+  //container.content.querySelector(".section-1__titulo-2").textContent =
+  // params.contentful;
   
-  const clone = document.importNode(template.content, true);
-  container.appendChild(clone);
+  //const clone = document.importNode(container.content, true);
+  //container.appendChild(clone);
    
-}
+//}
 
 
 function main(){
@@ -41,11 +40,11 @@ function main(){
   footerContent(footerEl);
 
 
-  getDesafio().then((res) => {
-    for (const w of res) {
-     addDesafio(w);
-    }
-  });
+ // getDesafio().then((res) => {
+   // for (const w of res) {
+     //addDesafio(w);
+   // }
+  //});
   
  
   }
