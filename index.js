9155fd3775dfba678401be275f2df42de9cde9cf
fefe <fedescalada17@hotmail.com>
2021-12-14 16:10:19 -0300

    contentful index 1

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-//function getDesafio(){
+function getDesafio(){
     
-  //return fetch("https://cdn.contentful.com/spaces/96le97k9rsgn/environments/master/entries?access_token=tHLOhPQdV9e9m5erEByoksWU_yGaj0eYQNOHzBR0vvc&content_type=desafio")
-  //.then(res =>{
-    //return res.json()
-  //}).then((data)=>{
-    //const fieldsCollections = data.items.map((item)=>{
-      //return{
-        //contentful: item.fields.contentful
+  return fetch("https://cdn.contentful.com/spaces/96le97k9rsgn/environments/master/entries?access_token=tHLOhPQdV9e9m5erEByoksWU_yGaj0eYQNOHzBR0vvc&content_type=desafio")
+  .then(res =>{
+    return res.json()
+  }).then((data)=>{
+    const fieldsCollections = data.items.map((item)=>{
+      return{
+        contentful: item.fields.contentful
 
-      //}
-  //})
-   // return fieldsCollections
-  //})
+      }
+  })
+    return fieldsCollections
+  })
 
-//}
+}
 
-//function addDesafio( params = {}){
-  //const container = document.querySelector(".section-1__titulo");
+function addDesafio( params = {}){
+  const container = document.querySelector(".section-1__titulo");
 
-  //container.content.querySelector(".section-1__titulo-1").textContent =
-  // params.contentful;
-  //container.content.querySelector(".section-1__titulo-2").textContent =
-  // params.contentful;
+  container.content.querySelector(".section-1__titulo-1").textContent =
+   params.contentful;
+  container.content.querySelector(".section-1__titulo-2").textContent =
+   params.contentful;
   
-  //const clone = document.importNode(container.content, true);
-  //container.appendChild(clone);
+  const clone = document.importNode(container.content, true);
+  container.appendChild(clone);
    
-//}
+}
 
 
 function main(){
