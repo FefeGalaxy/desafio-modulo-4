a116a02e7b6569480ec10b6da2eedf5a5b6a7c3f
fefe <fedescalada17@hotmail.com>
2021-12-14 21:07:52 -0300

    portfolio

--- index.js
+++ index.js
@@ -16,17 +16,51 @@ function getDesafio(){
 }
 
 function addDesafio( params = {}){
-  const container = document.querySelector(".section-1__titulo");
 
-  container.content.querySelector(".section-1__titulo-1").textContent =
+   const container = document.querySelector(".section-1__titulo-1").textContent =
    params.contentful;
-  container.content.querySelector(".section-1__titulo-2").textContent =
-   params.contentful;
+
   
   const clone = document.importNode(container.content, true);
   container.appendChild(clone);
    
 }
+
+
+
+
+
+function getPresentation(){
+    
+  return fetch("https://cdn.contentful.com/spaces/96le97k9rsgn/environments/master/entries?access_token=tHLOhPQdV9e9m5erEByoksWU_yGaj0eYQNOHzBR0vvc&content_type=presentation")
+  .then(res =>{
+    return res.json()
+  }).then((data)=>{
+    const fieldsCollections = data.items.map((item)=>{
+      return{
+        presentacion: item.fields.presentacion
+        
+      }
+  })
+    return fieldsCollections
+  })
+
+}
+
+function addPresentacion( params = {}){
+
+   const container = document.querySelector(".section-2__titulo").textContent =
+   params.presentacion;
+
+  
+  const clone = document.importNode(container.content, true);
+  container.appendChild(clone);
+   
+}
+
+
+
+
 
 
 function main(){
@@ -40,11 +74,16 @@ function main(){
   footerContent(footerEl);
 
 
- // getDesafio().then((res) => {
-   // for (const w of res) {
-     //addDesafio(w);
-   // }
-  //});
+  getDesafio().then((res) => {
+    for (const w of res) {
+     addDesafio(w);
+    }
+  });
+  getPresentation().then((res) => {
+    for (const w of res) {
+     addPresentacion(w);
+    }
+  });
   
  
   }
--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,28 @@
+function addPortfolio(){
+    const template = document.querySelector("#portfolio__section-template");
+    const container = document.querySelector(".portfolio__section");
+
+
+
+    const clone = document.importNode(template.content, true)
+    container.appendChild(clone);
+}
+
+
+
+
+
+
+
 function main () {
     const headerEl = document.querySelector(".header-content");
     headerContent(headerEl);
-
+    
+    
+    addPortfolio()
+    addPortfolio()
+    addPortfolio()
+    
     const footerEl = document.querySelector(".footer-content");
     footerContent(footerEl);
 }
